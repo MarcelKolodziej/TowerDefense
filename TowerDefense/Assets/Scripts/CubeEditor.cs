4b60c447aeb16de4ea2215b3a35658291743f201
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-09-14 23:02:59 +0200

    Labels
    Print coordinates of grid on the top of cubes.
    [SelectionBase] selecting whole object, avoid accidentally taking only top of cubes.

--- TowerDefense/Assets/Scripts/CubeEditor.cs
+++ TowerDefense/Assets/Scripts/CubeEditor.cs
@@ -8,16 +8,22 @@
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class PrintAwake : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
 
     void Update()
     {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
     }
 }
