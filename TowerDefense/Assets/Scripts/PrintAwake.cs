f27820b0960fa6565bc26eabee617bc116778715
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-09-14 22:25:21 +0200

    Coustom Script
    Coustom Script for grid size.

---
+++ TowerDefense/Assets/Scripts/PrintAwake.cs
@@ -0,0 +1,23 @@
+﻿// The PrintAwake script is placed on a GameObject.  The Awake function is
+// called when the GameObject is started at runtime.  The script is also
+// called by the Editor.  An example is when the Scene is changed to a
+// different Scene in the Project window.
+// The Update() function is called, for example, when the GameObject transform
+// position is changed in the Editor.
+
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PrintAwake : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
