44abf706ee7b949e6c6f94d074e1522de297383d
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-09-15 14:33:14 +0200

    List For Custom Path

---
+++ TowerDefense/Assets/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Block> path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintAllWayPoints();
+    }
+        private void PrintAllWayPoints() 
+        {
+            foreach (Block waypoint in path) 
+            {
+                print(waypoint.name);
+            }
+        
+        }
+
+    // Update is called once per frame
+    void Update() {
+  
+    }
+    
+}
