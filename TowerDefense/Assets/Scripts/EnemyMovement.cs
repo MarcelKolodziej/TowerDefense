4b6ed98a703bedf12739f4e8437a02ff4c37a469
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-09-17 20:10:41 +0200

    List
    Dictonary and List

--- TowerDefense/Assets/Scripts/EnemyMovement.cs
+++ TowerDefense/Assets/Scripts/EnemyMovement.cs
@@ -9,10 +9,8 @@ public class EnemyMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(FollowPath());
-        print("Hey I'm back at Start");
-    }
-
+        
+    }        
     IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in path)
---
+++ TowerDefense/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Dictionary<Vector2, Waypoint> grid = new Dictionary<Vector2, Waypoint>();
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogError("Skipping overlapping block" + waypoint);
+            }
+            else
+            {
+                    grid.Add(waypoint.GetGridPos(), waypoint);
+            }
+        }
+        print("Loaded " + grid.Count + " blocks");
+    }
+}
