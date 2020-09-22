a9ae9b2e3e1e3fa33a7cf3733c5423011f0acf5b
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-09-22 16:46:03 +0200

    Fixed bug

--- TowerDefense/Assets/Scripts/Waypoint.cs
+++ TowerDefense/Assets/Scripts/Waypoint.cs
@@ -14,7 +14,7 @@ public class Waypoint : MonoBehaviour
         return gridSize;
     }
 
-    public Vector2 GetGridPos()
+    public Vector2Int GetGridPos()
     {
        return new Vector2Int(
             Mathf.RoundToInt(transform.position.x / gridSize), // x  
